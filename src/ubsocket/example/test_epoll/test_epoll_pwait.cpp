54d811b200cd2e99879bb16acfc8192857b00314
Dubhep <panshuting@h-partners.com>
2026-01-13 09:03:26 +0800

    update: 更新文件 test_epoll_pwait.cpp
    Signed-off-by: Dubhep <panshuting@h-partners.com>

--- src/ubsocket/example/test_epoll/test_epoll_pwait.cpp
+++ src/ubsocket/example/test_epoll/test_epoll_pwait.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+* Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
 */
 #include <sys/epoll.h>
 #include <sys/socket.h>
