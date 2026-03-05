02bfd3c465e40490c20b833cba267c33aad6ebb3
gcw_GyK5eQlb <liushunbi@h-partners.com>
2026-03-05 19:22:15 +0800

    commit trace files
    Co-authored-by: gcw_GyK5eQlb<liushunbi@h-partners.com>

---
+++ src/ubsocket/trace/utracer_manager.h
@@ -0,0 +1,180 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ * Description: Provide the utility for cli message, etc
+ * Author:
+ * Create: 2026-03-02
+ * Note:
+ * History: 2026-03-02
+*/
+
+#ifndef UTRACER_MANAGER_H
+#define UTRACER_MANAGER_H
+
+#include <cerrno>
+#include <sys/cdefs.h>
+#include <iostream>
+#include <fcntl.h>
+#include <fstream>
+#include "utracer.h"
+#include "utracer_info.h"
+#include "rpc_adpt_vlog.h"
+
+namespace Statistics {
+class UTracerManager {
+public:
+    static __always_inline UTracerInfo *Instance()
+    {
+        static UTracerInfo *tracePoints = CreateInstance();
+        return tracePoints;
+    }
+
+    static __always_inline void DelayBegin(uint32_t tpId, const char *tpName)
+    {
+        auto instance = Instance();
+        if (tpId >= MAX_TRACE_POINT_NUM) {
+            return;
+        }
+        instance[tpId].DelayBegin(tpName);
+    }
+
+    static __always_inline void DelayEnd(uint32_t tpId, const uint64_t diff, int32_t retCode)
+    {
+        auto instance = Instance();
+        if (tpId >= MAX_TRACE_POINT_NUM) {
+            return;
+        }
+        if (mDumpEnable) {
+            DumpTraceSplitInfo(instance[tpId].GetName(), diff, retCode);
+        }
+        instance[tpId].DelayEnd(diff, retCode, mLatencyQuantileEnable);
+    }
+
+    static __always_inline timespec AsyncDelayBegin(uint32_t tpId, const char *tpName)
+    {
+        DelayBegin(tpId, tpName);
+        struct timespec tpDelay = {0, 0};
+        clock_gettime(CLOCK_MONOTONIC, &tpDelay);
+        return tpDelay;
+    }
+
+    static __always_inline uint64_t GetTimeNs()
+    {
+        struct timespec tpDelay = {0, 0};
+        clock_gettime(CLOCK_MONOTONIC, &tpDelay);
+        return tpDelay.tv_sec * 1000000000ULL + tpDelay.tv_nsec;
+    }
+
+    static __always_inline void SetEnable(bool enable)
+    {
+        mEnable = enable;
+    }
+
+    static __always_inline bool IsEnable()
+    {
+        return mEnable;
+    }
+
+    static __always_inline void SetLatencyQuantileEnable(bool enable)
+    {
+        mLatencyQuantileEnable = enable;
+    }
+
+    static __always_inline bool IsLatencyQuantileEnable()
+    {
+        return mLatencyQuantileEnable;
+    }
+
+    static __always_inline void SetEnableLog(bool enable, std::string logPath)
+    {
+        if (!logPath.empty() && !mDumpEnable) {
+            GetLogPath(logPath);
+        }
+        if (enable && mDumpDir.empty()) {
+            int32_t ret = UTracerUtils::CreateDirectory(mDefaultDir);
+            if (ret != 0) {
+                RPC_ADPT_VLOG_WARN("[UTRACER], prepare dum dir failed, disable dump feature!");
+            }
+            mDumpDir = mDefaultDir;
+        }
+        mDumpEnable = enable;
+    }
+
+    static __always_inline bool IsEnableLog()
+    {
+        return mDumpEnable;
+    }
+
+private:
+    static __always_inline UTracerInfo *CreateInstance()
+    {
+        UTracerInfo *instance = new (std::nothrow) UTracerInfo [MAX_TRACE_POINT_NUM];
+        if (instance == nullptr) {
+            return nullptr;
+        } else {
+            auto ret = memset_s(instance, sizeof(UTracerInfo) * MAX_TRACE_POINT_NUM,
+                                0x0, sizeof(UTracerInfo) * MAX_TRACE_POINT_NUM);
+            if (ret != 0) {
+                RPC_ADPT_VLOG_WARN("[UTRACER] Failed to memset_s to instance.");
+                delete[] instance;
+                instance = nullptr;
+                return nullptr;
+            }
+        }
+        return instance;
+    }
+
+    static __always_inline void DumpTraceSplitInfo(std::string tpName, const uint64_t diff, int32_t retCode)
+    {
+        std::stringstream ss;
+        std::string currentTime = UTracerUtils::CurrentTime();
+        ss << currentTime << "|" << tpName << "|" << retCode << "|" << diff << "(ns)" << std::endl;
+
+        std::string dumpPath = mDumpDir + "/utrace_" + std::to_string(getpid()) + ".log";
+        int fd = open(dumpPath.c_str(), O_WRONLY | O_CREAT | O_APPEND, 0640);
+        if (fd == -1) {
+            return;
+        }
+
+        std::ofstream dump;
+        dump.open(dumpPath, std::ios::out | std::ios::app);
+        if (!dump.is_open()) {
+            close(fd);
+            return;
+        }
+
+        dump << ss.str();
+        dump.flush();
+        dump.close();
+        close(fd);
+    }
+
+    static void GetLogPath(std::string &path)
+    {
+        if (!UTracerUtils::CanonicalPath(path)) {
+            RPC_ADPT_VLOG_WARN("[UTRACER] Log directory is invalid, use default path. ");
+            /* path is error, use old path */
+            if (!mDumpDir.empty()) {
+                return;
+            }
+            /* if path error and old path is empty, use default path */
+            int32_t ret = UTracerUtils::CreateDirectory(mDefaultDir);
+            if (ret != 0) {
+                RPC_ADPT_VLOG_WARN("[UTRACER] prepare dump dir failed, disable dump feature!");
+            }
+            mDumpDir = mDefaultDir;
+            return;
+        }
+        mDumpDir = path;
+        return;
+    }
+
+private:
+    static bool mEnable;
+    static bool mLatencyQuantileEnable;
+    static bool mDumpEnable;
+    static std::string mDumpDir;
+    static std::string mDefaultDir;
+};
+}
+
+#endif // UTRACER_MANAGER_H
---
+++ src/ubsocket/trace/utracer_utils.h
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ * Description: Provide the utility for cli message, etc
+ * Author:
+ * Create: 2026-03-02
+ * Note:
+ * History: 2026-03-02
+*/
+
+#ifndef UTRACER_UTILS_H
+#define UTRACER_UTILS_H
+
+#include <vector>
+#include <cstring>
+#include <memory>
+#include <algorithm>
+#include <iomanip>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sstream>
+#include <linux/limits.h>
+#include "securec.h"
+#include "utracer_tdigest.h"
+#include "utracer_def.h"
+
+namespace Statistics {
+class UTracerUtils {
+public:
+    static std::string &StrTrim(std::string &str)
+    {
+        if (str.empty()) {
+            return str;
+        }
+        str.erase(0, str.find_first_not_of(" "));
+        str.erase(str.find_last_not_of(" ") + 1);
+        return str;
+    }
+
+    static std::string CurrentTime()
+    {
+        time_t rawTime;
+        (void)time(&rawTime);
+        auto tmInfo = localtime(&rawTime);
+        if (tmInfo == nullptr) {
+            return "";
+        }
+        std::stringstream ss;
+        ss << std::setfill('0') << std::setw(NN_NO4) << std::right << (NN_NO1900 + tmInfo->tm_year) << "-" <<
+        std::setfill('0') << std::setw(NN_NO2) << std::right << (NN_NO1 + tmInfo->tm_mon) << "-" <<
+        std::setfill('0') << std::setw(NN_NO2) << std::right << tmInfo->tm_mday << " " <<
+        std::setfill('0') << std::setw(NN_NO2) << std::right << tmInfo->tm_hour << ":" <<
+        std::setfill('0') << std::setw(NN_NO2) << std::right << tmInfo->tm_min << ":" <<
+        std::setfill('0') << std::setw(NN_NO2) << std::right << tmInfo->tm_sec;
+        return ss.str();
+    }
+
+    static std::string FormatString(std::string &name, uint64_t begin, uint64_t goodEnd, uint64_t badEnd, uint64_t min,
+        uint64_t max, uint64_t total)
+    {
+        std::string str;
+        std::ostringstream os(str);
+        os.flags(std::ios::fixed);
+        os.precision(NN_NO3);
+        auto unitStep = NN_NO1000;
+        os << std::left << std::setw(NN_NO50) << name
+           << "\t" << std::left << std::setw(NN_NO15) << begin
+           << "\t" << std::left << std::setw(NN_NO15) << goodEnd
+           << "\t" << std::left << std::setw(NN_NO15) << badEnd
+           << "\t" << std::left << std::setw(NN_NO15) << ((begin > goodEnd - badEnd) ? (begin - goodEnd - badEnd) : 0)
+           << "\t" << std::left << std::setw(NN_NO15) << (min == UINT64_MAX ? 0 : ((double)min / unitStep))
+           << "\t" << std::left << std::setw(NN_NO15) << (double)max / unitStep
+           << "\t" << std::left << std::setw(NN_NO15) << (goodEnd == 0 ? 0 : (double)total / goodEnd / unitStep)
+           << "\t" << std::left << std::setw(NN_NO15) << (double)total / unitStep;
+        return os.str();
+    }
+
+    static void SplitStr(const std::string &str, const std::string &separator, std::vector<std::string> &result)
+    {
+        result.clear();
+        std::string::size_type pos1 = 0;
+        std::string::size_type pos2 = str.find(separator);
+
+        std::string tmpStr;
+        while (pos2 != std::string::npos) {
+            tmpStr = str.substr(pos1, pos2 - pos1);
+            result.emplace_back(tmpStr);
+            pos1 = pos2 + separator.size();
+            pos2 = str.find(separator, pos1);
+        }
+
+        if (pos1 != str.length()) {
+            tmpStr = str.substr(pos1);
+            result.emplace_back(tmpStr);
+        }
+    }
+
+    static int CreateDirectory(const std::string &name)
+    {
+        std::vector<std::string> paths;
+        SplitStr(name, "/", paths);
+        int32_t ret = 0;
+        std::string pathTmp;
+        for (auto &item : paths) {
+            if (item.empty()) {
+                continue;
+            }
+
+            pathTmp += "/" + item;
+            if (access(pathTmp.c_str(), F_OK) != 0) {
+                mode_t old_mask = umask(0);
+                ret = mkdir(pathTmp.c_str(), S_IRWXU | S_IRGRP | S_IXGRP);
+                umask(old_mask);
+                if (ret != 0 && errno != EEXIST) {
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+
+    /* *
+     * @brief Check whether the path is canonical, and canonical it.
+     */
+    static bool CanonicalPath(std::string &path)
+    {
+        if (path.empty() || path.size() > PATH_MAX) {
+            return false;
+        }
+
+        /* It will allocate memory to store path */
+        char *realPath = realpath(path.c_str(), nullptr);
+        if (realPath == nullptr) {
+            return false;
+        }
+
+        path = realPath;
+        free(realPath);
+        realPath = nullptr;
+        return true;
+    }
+};
+}
+#endif // UTRACER_UTILS_H
