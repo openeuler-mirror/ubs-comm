04c1e11786fb9f2191aab26457252ee460075296
gcw_GyK5eQlb <liushunbi@h-partners.com>
2026-05-21 10:34:15 +0800

    fix socket error

--- src/ubsocket/csrc/ubsocket_sock.cpp
+++ src/ubsocket/csrc/ubsocket_sock.cpp
@@ -133,7 +133,7 @@ UBS_API int UB_API_WRAP(listen)(int fd, int backlog)
 
 UBS_API int UB_API_WRAP(connect)(int fd, const struct sockaddr *address, socklen_t address_len)
 {
-    if (!GlobalSetting::UBS_NATIVE_TCP_MODE) {
+    if (GlobalSetting::UBS_NATIVE_TCP_MODE) {
         return LibcApi::connect(fd, address, address_len);
     }
     SocketPtr sock = SocketSet::Instance().GetSocket(fd);
